2e846ff4609d34f47d3aaf5c842c6361395228e3
Lincoln Berkley <linc.berkley@yale.edu>
2018-12-14 15:14:19 -0500

    Make story box auto scroll

--- src/StoryBox.js
+++ src/StoryBox.js
@@ -4,13 +4,15 @@ class StoryBox extends Component {
   render() {
     return (
       <div className="story-box-wrapper">
-        <div className="story-box">{this.props.contents}</div>
+        <div className="story-box" ref={n => this.box = n}>
+          {this.props.contents}
+        </div>
       </div>
     )
   }
 
   componentDidUpdate() {
-
+    this.box.scrollTop = this.box.scrollHeight;
   }
 }
 
