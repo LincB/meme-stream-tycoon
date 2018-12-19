2025cfb3c32c99a817450633aabc2877a0d0aaeb
Lincoln Berkley <linc.berkley@yale.edu>
2018-12-19 16:11:46 -0500

    Try again to fix resource loading

---
+++ docs/precache-manifest.f691c9f29ab3153c9a166dba4d859188.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56875ac1c19cd027c48b",
+    "url": "/meme-stream-tycoon/static/js/runtime~main.56875ac1.js"
+  },
+  {
+    "revision": "9155420e80cd3673a814",
+    "url": "/meme-stream-tycoon/static/js/main.9155420e.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/meme-stream-tycoon/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "9155420e80cd3673a814",
+    "url": "/meme-stream-tycoon/static/css/main.3c8985b4.chunk.css"
+  },
+  {
+    "revision": "64010773003b6820023a648ee43248cc",
+    "url": "/meme-stream-tycoon/index.html"
+  }
+];
